e16e2bf8b31a1eeacfbf11d564ee8ce59e5bbb27
jorpelu <jorgeperez96jpl@gmail.com>
2023-09-16 19:56:44 +0200

    Updates

--- src/components/PrecioFlotante.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="position-fixed end-0 bottom-0 m-5 ">
-        20.20€
-    </div>
-</template>
--- src/views/Error/NotFoundView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <h1>Error 404</h1>
-        <p>Page not found</p>
-    </div>
-</template>
