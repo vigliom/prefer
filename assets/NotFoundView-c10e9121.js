4e741737c2784282bde2141a853fed5c3b02ac33
jorpelu <jorgeperez96jpl@gmail.com>
2023-09-17 18:34:36 +0200

    Updates

---
+++ assets/NotFoundView-c10e9121.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-c27b6911.js";import{o as t,c as n,d as o}from"./index-990deeeb.js";const c={},r=o("h1",null,"Error 404",-1),s=o("p",null,"Page not found",-1),_=[r,s];function a(d,l){return t(),n("div",null,_)}const u=e(c,[["render",a]]);export{u as default};
